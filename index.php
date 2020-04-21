e41afc436932c2354d6646ebe8b95924863f170d
WTT CIS <withan@kkumail.com>
2020-04-22 04:55:41 +0700

    create new

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THAINAPIER เว็บไทยเนเปียร์</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/bb7f694074.js"></script>
+
+
+</head>
+<body>
+
+  <header>
+      <!-- <img src="img/logo_napier.png" width="100%" alt="" srcset=""> -->
+       <header>
+        <nav>
+          <input type="checkbox" id="check">
+          <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+          </label>
+          
+          
+           <label class="logo">THAINAPIER</label> 
+            <ul>
+              <li><a href="#" class="active">ข้อมูลหญ้าเนเปียร์</a></li>
+              <li><a href="#">การเพาะปลูก</a></li>
+              <li><a href="#">การทดลอง</a></li>
+              <li><a href="#">คำนวณต้นทุน</a></li>
+              <li><a href="#">ลงทะเบียนเกษตรกร</a></li>
+            </ul>
+        </nav>
+     
+
+  </header>
+
+  <section class="content">
+
+    <div class="container">
+      <div class="content-about-napier">
+          <h1>หญ้าเนเปียร์ (Napier Grass)</h1>
+      </div>
+
+      <div class="content-planting-napier">
+          <h1>การเพาะปลูกหญ้าเนเปียร์ </h1>
+      </div>
+    </div>
+  </section>
+
+  <!--<footer>
+    <div class="container">
+        <div class="credit">
+          <h4>เว็บไซต์นี้จัดทำโดยนักศึกษาสาขาวิทยาการคอมพิวเตอร์และสารสนเทศ</h4>
+        </div>
+    </div>
+  </footer> -->
+
+</body>
+</html>
